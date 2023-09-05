5281ac5f5aef4c01f2f82f182ff8bdd72765b14e
Nargizi <ggoge19@freeuni.edu.ge>
2023-09-05 14:54:52 +0400

    add use instruction

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from src.postagger.model import POSTagger
-
-
-@pytest.fixture
-def tagger() -> POSTagger:
-    return POSTagger()
-
--- tests/test_postagger.py
+++
@@ -1,7 +0,0 @@
-from src.postagger.model import POSTagger
-
-
-def test_postagger(tagger: POSTagger) -> None:
-    assert tagger('მე მივდივარ სახლში') == ['Pron', 'V', 'N']
-
-
